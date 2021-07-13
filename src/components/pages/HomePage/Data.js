38ce0836a5c055d3e8a6107e2e97d3986939857d
Kayte <kaytemcdonough@gmail.com>
2021-07-13 19:37:15 -0400

    started HeroSection

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Exclusive Access",
+  headline: "Unlimited Transactions with Zero Fees",
+  description: "Get access to our exclusive art show.",
+  buttonLabel: "Get Started",
+  imgStart: "",
+  img: "images/svg-1.svg",
+  alt: "credit card",
+};
