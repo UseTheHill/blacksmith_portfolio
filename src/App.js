b2f66360560bad24962ed791b10ea9b604d62065
Kayte <kaytemcdonough@gmail.com>
2021-07-14 18:36:36 -0400

    finished footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/HomePage/Home";
+import Footer from "./components/pages/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -7,7 +7,7 @@ import {
   FaInstagram,
   FaYoutube,
   FaTwitter,
-  FaLinkedIn,
+  FaLinkedin,
 } from "react-icons/fa";
 import { GiAnvilImpact } from "react-icons/gi";
 
@@ -31,6 +31,92 @@ function Footer() {
             />
             <Button buttonStyle="btn--outline">Subscribe</Button>
           </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact</h2>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Sponsorships</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Videos</h2>
+            <Link to="/">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influencer</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Youtube</Link>
+            <Link to="/">Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              <GiAnvilImpact className="navbar-icon" />
+              Blacksmith
+            </Link>
+          </div>
+          <small className="website-rights">McDonough &#169; 2021</small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <FaFacebook />
+            </Link>
+            <Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <FaInstagram />
+            </Link>
+            <Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <FaYoutube />
+            </Link>
+            <Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <FaTwitter />
+            </Link>
+            <Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin />
+            </Link>
+          </div>
         </div>
       </section>
     </div>
