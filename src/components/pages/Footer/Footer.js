a90d4a2ffb9e8da462fec9280614b2c4c2c23b8e
Kayte <kaytemcdonough@gmail.com>
2021-07-14 12:04:21 -0400

    began js on footer

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import { Button } from "../../Button";
+import { Link } from "react-router-dom";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedIn,
+} from "react-icons/fa";
+import { GiAnvilImpact } from "react-icons/gi";
 
 function Footer() {
-    return (
-        <div>
-            
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription-heading">
+        <p className="footer-subscription-heading">
+          Join our email list to get updates on new projects completed.
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time.
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              className="footer-input"
+              name="email"
+              type="email"
+              placeholder="Your email"
+            />
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
